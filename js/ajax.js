1dcd108bc3547c125597f2b459f6c621bd63a854
Abel Gehm <abelgehm@gmail.com>
2019-11-30 22:51:49 -0300

    Backdrop

--- js/ajax.js
+++ js/ajax.js
@@ -3,11 +3,30 @@ function CarregarTabela(tabela, titulo="Cashback plus") {
   $('#mdlb').load(tabela, function() {$('#mdl').modal('show');});
 }
 
-$('#mdlx').click(LimpaModal);
-$('#mdlf').click(LimpaModal)
-
 function LimpaModal(){
   $('#mdlt').html("Cashback plus");
   $('#mdlb').html("");
 }
 
+function Backdrop(ativar){
+  if (ativar){
+    $('#cover-spin').show();
+  }
+  else{
+    $('#cover-spin').hide();
+  }
+}
+
+$('#mdlx').click(LimpaModal);
+$('#mdlf').click(LimpaModal)
+
+
+$('#mdl').on('show.bs.modal', function(){Backdrop(false);})
+
+$('#btnOndeComprar').click(MostraOndeComprar)
+
+function MostraOndeComprar() {
+  Backdrop(true);
+  CarregarTabela('https://escritorio.rcirenda.com.br/ondecomprar', 'Confira os estabelecimentos conveniados');
+  return false;
+}
